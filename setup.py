2d7c72fc7ea2ae228156219abb18ccfdc6c74c64
Thomas Massmann <thomas@propertyshelf.com>
2013-10-17 14:15:45 +0200

    Use rst instead of txt. Updated setup.py content.

--- setup.py
+++ setup.py
@@ -1,41 +1,46 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = '0.1dev'
 
-setup(name='theming.toolkit.templates',
-      version=version,
-      description="A product that allows to change a set of templates in the plone.app.theming configuration",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
+setup(
+    name='theming.toolkit.templates',
+    version=version,
+    description='A product that allows to change a set of templates in the ' \
+                'plone.app.theming configuration',
+    long_description=open('README.rst').read() + '\n' +
+                     open(os.path.join('docs', 'HISTORY.txt')).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='Jens Krause',
-      author_email='jens@propertyshelf.com',
-      url='http://propertyshelf.com',
-      license='GPL',
-      packages=find_packages('src', exclude=['ez_setup']),
-      namespace_packages=['theming', 'theming.toolkit'],
-      package_dir={'': 'src'},
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.app.theming',
-          # -*- Extra requirements: -*-
-      ],
-      extras_require={
-             'test': ['plone.app.testing',]
-      },
-      entry_points="""
-      # -*- Entry points: -*-
+    ],
+    keywords='plone theming',
+    author='Jens Krause',
+    author_email='jens@propertyshelf.com',
+    url='https://github.com/propertyshelf/theming.toolkit.templates',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['theming', 'theming.toolkit'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.app.theming',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+        ]
+    },
+    entry_points="""
+    # -*- Entry points: -*-
 
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
